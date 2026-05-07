94fd2f34a38c63f8fce443a3775355ffaa91734d
2bit <i@2bit.jp>
2026-05-07 12:44:02 +0900

    fix: sync enum values with C API (3ch format additions shifted all values)
    NozzleTextureFormat gained 5 new RGB variants (RGB8_UNORM, RGB16_UNORM, RGB16_FLOAT, RGB32_FLOAT, RGB32_UINT) which shifted all subsequent values by +1 to +5. Updated Java TextureFormat enum to match. Also updated bytesPerPixel() for the new formats.

--- src/main/java/nozzle/TextureFormat.java
+++ src/main/java/nozzle/TextureFormat.java
@@ -4,22 +4,27 @@ public enum TextureFormat {
     UNKNOWN(0),
     R8_UNORM(1),
     RG8_UNORM(2),
-    RGBA8_UNORM(3),
-    BGRA8_UNORM(4),
-    RGBA8_SRGB(5),
-    BGRA8_SRGB(6),
-    R16_UNORM(7),
-    RG16_UNORM(8),
-    RGBA16_UNORM(9),
-    R16_FLOAT(10),
-    RG16_FLOAT(11),
-    RGBA16_FLOAT(12),
-    R32_FLOAT(13),
-    RG32_FLOAT(14),
-    RGBA32_FLOAT(15),
-    R32_UINT(16),
-    RGBA32_UINT(17),
-    DEPTH32_FLOAT(18);
+    RGB8_UNORM(3),
+    RGBA8_UNORM(4),
+    BGRA8_UNORM(5),
+    RGBA8_SRGB(6),
+    BGRA8_SRGB(7),
+    R16_UNORM(8),
+    RG16_UNORM(9),
+    RGB16_UNORM(10),
+    RGBA16_UNORM(11),
+    R16_FLOAT(12),
+    RG16_FLOAT(13),
+    RGB16_FLOAT(14),
+    RGBA16_FLOAT(15),
+    R32_FLOAT(16),
+    RG32_FLOAT(17),
+    RGB32_FLOAT(18),
+    RGBA32_FLOAT(19),
+    R32_UINT(20),
+    RGBA32_UINT(21),
+    RGB32_UINT(22),
+    DEPTH32_FLOAT(23);
 
     private final int value;
 
@@ -46,6 +51,7 @@ public enum TextureFormat {
             case RG8_UNORM:
             case R16_UNORM:
             case R16_FLOAT: return 2;
+            case RGB8_UNORM:
             case RGBA8_UNORM:
             case BGRA8_UNORM:
             case RGBA8_SRGB:
@@ -55,11 +61,14 @@ public enum TextureFormat {
             case R32_FLOAT:
             case R32_UINT:
             case DEPTH32_FLOAT: return 4;
+            case RGB16_UNORM:
             case RGBA16_UNORM:
             case RGBA16_FLOAT:
             case RG32_FLOAT: return 8;
+            case RGB32_FLOAT:
             case RGBA32_FLOAT:
             case RGBA32_UINT: return 16;
+            case RGB32_UINT: return 12;
             default: return 0;
         }
     }
